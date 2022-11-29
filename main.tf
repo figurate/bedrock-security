cec7f6f306ac665607632c04bb19575266c32683
Ben Fortuna <fortuna@micronode.com>
2022-11-30 09:23:06 +1100

    Refactoring

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "aws-analyzer-baseline" {
-  source = "./access-analyzer-baseline"
+  source = "./aws-analyzer-baseline"
   count = var.access_analyzer_baseline_enabled ? 1 : 0
 }
 
@@ -9,31 +9,31 @@ module "aws-config-baseline" {
 }
 
 module "aws-cloudtrail-baseline" {
-  source = "./cloudtrail-baseline"
+  source = "./aws-cloudtrail-baseline"
   count = var.cloudtrail_baseline_enabled ? 1 : 0
 }
 
 module "aws-alarm-baseline" {
-  source = "./cloudwatch-alarm-baseline"
+  source = "./aws-alarm-baseline"
   count = var.cloudwatch_baseline_enabled ? 1 : 0
 }
 
 module "aws-ebs-baseline" {
-  source = "./ebs-baseline"
+  source = "./aws-ebs-baseline"
   count = var.ebs_baseline_enabled ? 1 : 0
 }
 
 module "aws-guardduty-baseline" {
-  source = "./guardduty-baseline"
+  source = "./aws-guardduty-baseline"
   count = var.guardduty_baseline_enabled ? 1 : 0
 }
 
 module "aws-iam-baseline" {
-  source = "./iam-baseline"
+  source = "./aws-iam-baseline"
   count = var.iam_baseline_enabled ? 1 : 0
 }
 
 module "aws-s3-baseline" {
-  source = "./s3-baseline"
+  source = "./aws-s3-baseline"
   count = var.s3_baseline_enabled ? 1 : 0
 }
